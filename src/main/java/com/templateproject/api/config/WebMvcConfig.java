069f2a9d52bfe6bc3e2043846e3263bee61fdbf3
smaile <smail_lakermi@yahoo.fr>
2023-08-02 11:33:05 +0200

    init project

---
+++ src/main/java/com/templateproject/api/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+/*package com.templateproject.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+/**
+ * *
+ * @author smaile
+ *
+ */
+/*
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+        .allowedOrigins("http://localhost:4200")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+        .allowedHeaders("Content-Type", "Date", "Total-Count", "loginInfo", "Authorization", "x-token")
+        .allowCredentials(true);
+    }
+}*/
---
+++ src/main/java/com/templateproject/api/filter/AuthFilter.java
@@ -0,0 +1,53 @@
+package com.templateproject.api.filter;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import com.templateproject.api.service.*;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class AuthFilter implements Filter {
+	
+    private final AuthService authService;
+    
+    public AuthFilter(AuthService authService) {
+        this.authService = authService;
+    }
+    
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        var request = (HttpServletRequest) servletRequest;
+        var response = (HttpServletResponse) servletResponse;
+        
+        // Check for OPTIONS request  pour corriger l erreur 
+
+     /*   response.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        response.addHeader("Access-Control-Allow-Headers", "*");
+        response.addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST, DELETE");
+        var path = request.getServletPath();
+        if (!path.equals("/auth/register2") && !path.equals("/auth/login")) {
+            var token = request.getHeader("x-token");
+            System.out.println("TOKEN IS : " + token);
+            Integer playerID = authService.findPlayerIdByToken(token);
+        	//Integer playerID = 1;
+            if (playerID == null) {
+                response.setHeader("Content-type", "application/json");
+                response.setStatus(401);
+                response.getWriter().println("{" +
+                        "\"message\" : \"Invalid Token\"" +
+                        "}");
+                return;
+            }
+           request.setAttribute("playerID", playerID);
+        }*/
+        chain.doFilter(request, response);
+    }
+} 

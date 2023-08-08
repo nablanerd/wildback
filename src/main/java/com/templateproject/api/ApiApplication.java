d5d8c2c5a5dc7fe22bdef8c7996f0ef55bc92f60
nablanerd <nablanerd@yahoo.com>
2023-08-08 21:41:35 +0200

    generate building and player

--- src/main/java/com/templateproject/api/ApiApplication.java
+++ src/main/java/com/templateproject/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package com.templateproject.api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,13 +8,30 @@ import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.templateproject.api.service.AuthService;
+
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication implements CommandLineRunner{
+
+private AuthService authService;
+
+	public ApiApplication(AuthService authService){
+this.authService =authService;
+
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+authService.register("a","a","a","a");
+authService.register("b","b","b","b");
+
+	}
 	/*@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
